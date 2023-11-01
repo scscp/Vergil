141581ef50eb82c8bda6b7679dbba2560e003933
scscp <143437421+scscp@users.noreply.github.com>
2023-11-02 04:52:38 +0700

    Add files via upload

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -20,7 +20,7 @@ commonname=none
 email=elliez667@gmail.com
 
 # simple password minimal
-curl -sS https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/ssh/password | openssl aes-256-cbc -d -a -pass pass:scvps07gg -pbkdf2 > /etc/pam.d/common-password
+curl -sS https://raw.githubusercontent.com/scscp/Vergil/main2/ssh/password | openssl aes-256-cbc -d -a -pass pass:scvps07gg -pbkdf2 > /etc/pam.d/common-password
 chmod +x /etc/pam.d/common-password
 
 # go to root
@@ -128,13 +128,13 @@ apt -y install nginx
 cd
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/ssh/nginx.conf"
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/scscp/Vergil/main2/ssh/nginx.conf"
 mkdir -p /home/vps/public_html
 /etc/init.d/nginx restart
 
 # install badvpn
 cd
-wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/ssh/newudpgw"
+wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/scscp/Vergil/main2/ssh/newudpgw"
 chmod +x /usr/bin/badvpn-udpgw
 sed -i '$ i\screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500' /etc/rc.local
 sed -i '$ i\screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500' /etc/rc.local
@@ -243,13 +243,13 @@ echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
 # banner /etc/issue.net
 sleep 1
 echo -e "[ ${green}INFO$NC ] Settings banner"
-wget -q -O /etc/issue.net "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/issue.net"
+wget -q -O /etc/issue.net "https://raw.githubusercontent.com/scscp/Vergil/main/issue.net"
 chmod +x /etc/issue.net
 echo "Banner /etc/issue.net" >> /etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
 
 #install bbr dan optimasi kernel
-wget https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/ssh/bbr.sh && chmod +x bbr.sh && ./bbr.sh
+wget https://raw.githubusercontent.com/scscp/Vergil/main2/ssh/bbr.sh && chmod +x bbr.sh && ./bbr.sh
 
 # blockir torrent
 iptables -A FORWARD -m string --string "get_peers" --algo bm -j DROP
@@ -271,58 +271,58 @@ netfilter-persistent reload
 # download script
 cd /usr/bin
 # menu
-wget -O menu "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/menu/menu.sh"
-wget -O menu-trial "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/menu/menu-trial.sh"
-wget -O menu-vmess "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/menu/menu-vmess.sh"
-wget -O menu-vless "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/menu/menu-vless.sh"
-wget -O running "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/menu/running.sh"
-wget -O clearcache "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/menu/clearcache.sh"
-wget -O menu-trgo "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/menu/menu-trgo.sh"
-wget -O menu-trojan "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/menu/menu-trojan.sh"
-wget -O cf "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/cf.sh"
+wget -O menu "https://raw.githubusercontent.com/scscp/Vergil/main/menu/menu.sh"
+wget -O menu-trial "https://raw.githubusercontent.com/scscp/Vergil/main/menu/menu-trial.sh"
+wget -O menu-vmess "https://raw.githubusercontent.com/scscp/Vergil/main/menu/menu-vmess.sh"
+wget -O menu-vless "https://raw.githubusercontent.com/scscp/Vergil/main/menu/menu-vless.sh"
+wget -O running "https://raw.githubusercontent.com/scscp/Vergil/main2/menu/running.sh"
+wget -O clearcache "https://raw.githubusercontent.com/scscp/Vergil/main/menu/clearcache.sh"
+wget -O menu-trgo "https://raw.githubusercontent.com/scscp/Vergil/main2/menu/menu-trgo.sh"
+wget -O menu-trojan "https://raw.githubusercontent.com/scscp/Vergil/main/menu/menu-trojan.sh"
+wget -O cf "https://raw.githubusercontent.com/scscp/Vergil/main/cf.sh"
 
 # menu ssh ovpn
-wget -O menu-ssh "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/menu/menu-ssh.sh"
-wget -O usernew "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/ssh/usernew.sh"
-wget -O trial "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/ssh/trial.sh"
-wget -O renew "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/ssh/renew.sh"
-wget -O hapus "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/ssh/hapus.sh"
-wget -O cek "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/ssh/cek.sh"
-wget -O member "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/ssh/member.sh"
-wget -O delete "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/ssh/delete.sh"
-wget -O autokill "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/ssh/autokill.sh"
-wget -O ceklim "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/ssh/ceklim.sh"
-wget -O tendang "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/ssh/tendang.sh"
+wget -O menu-ssh "https://raw.githubusercontent.com/scscp/Vergil/main/menu/menu-ssh.sh"
+wget -O usernew "https://raw.githubusercontent.com/scscp/Vergil/main/ssh/usernew.sh"
+wget -O trial "https://raw.githubusercontent.com/scscp/Vergil/main/ssh/trial.sh"
+wget -O renew "https://raw.githubusercontent.com/scscp/Vergil/main2/ssh/renew.sh"
+wget -O hapus "https://raw.githubusercontent.com/scscp/Vergil/main2/ssh/hapus.sh"
+wget -O cek "https://raw.githubusercontent.com/scscp/Vergil/main2/ssh/cek.sh"
+wget -O member "https://raw.githubusercontent.com/scscp/Vergil/main2/ssh/member.sh"
+wget -O delete "https://raw.githubusercontent.com/scscp/Vergil/main2/ssh/delete.sh"
+wget -O autokill "https://raw.githubusercontent.com/scscp/Vergil/main2/ssh/autokill.sh"
+wget -O ceklim "https://raw.githubusercontent.com/scscp/Vergil/main2/ssh/ceklim.sh"
+wget -O tendang "https://raw.githubusercontent.com/scscp/Vergil/main2/ssh/tendang.sh"
 
 # menu system
-wget -O menu-set "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/menu/menu-set.sh"
-wget -O menu-domain "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/menu/menu-domain.sh"
-wget -O add-host "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/ssh/add-host.sh"
-wget -O port-change "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/port/port-change.sh"
-wget -O certv2ray "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/xray/certv2ray.sh"
-wget -O menu-webmin "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/menu/menu-webmin.sh"
-wget -O speedtest "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/ssh/speedtest_cli.py"
-wget -O about "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/menu/about.sh"
-wget -O auto-reboot "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/menu/auto-reboot.sh"
-wget -O restart "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/menu/restart.sh"
-wget -O bw "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/menu/bw.sh"
+wget -O menu-set "https://raw.githubusercontent.com/scscp/Vergil/main/menu/menu-set.sh"
+wget -O menu-domain "https://raw.githubusercontent.com/scscp/Vergil/main2/menu/menu-domain.sh"
+wget -O add-host "https://raw.githubusercontent.com/scscp/Vergil/main2/ssh/add-host.sh"
+wget -O port-change "https://raw.githubusercontent.com/scscp/Vergil/main2/port/port-change.sh"
+wget -O certv2ray "https://raw.githubusercontent.com/scscp/Vergil/main2/xray/certv2ray.sh"
+wget -O menu-webmin "https://raw.githubusercontent.com/scscp/Vergil/main2/menu/menu-webmin.sh"
+wget -O speedtest "https://raw.githubusercontent.com/scscp/Vergil/main2/ssh/speedtest_cli.py"
+wget -O about "https://raw.githubusercontent.com/scscp/Vergil/main/menu/about.sh"
+wget -O auto-reboot "https://raw.githubusercontent.com/scscp/Vergil/main2/menu/auto-reboot.sh"
+wget -O restart "https://raw.githubusercontent.com/scscp/Vergil/main2/menu/restart.sh"
+wget -O bw "https://raw.githubusercontent.com/scscp/Vergil/main2/menu/bw.sh"
 
 # change port
-wget -O port-ssl "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/port/port-ssl.sh"
-wget -O port-ovpn "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/port/port-ovpn.sh"
+wget -O port-ssl "https://raw.githubusercontent.com/scscp/Vergil/main2/port/port-ssl.sh"
+wget -O port-ovpn "https://raw.githubusercontent.com/scscp/Vergil/main2/port/port-ovpn.sh"
 
 
-wget -O xp "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/ssh/xp.sh"
-wget -O acs-set "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/acs-set.sh"
+wget -O xp "https://raw.githubusercontent.com/scscp/Vergil/main2/ssh/xp.sh"
+wget -O acs-set "https://raw.githubusercontent.com/scscp/Vergil/main2/acs-set.sh"
 
-wget -O sshws "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/ssh/sshws.sh"
-wget -O status "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/status.sh"
-wget -O menu-backup "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/backup/menu-backup.sh"
-wget -O backup "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/backup/backup.sh"
-wget -O restore "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/backup/restore.sh"
+wget -O sshws "https://raw.githubusercontent.com/scscp/Vergil/main2/ssh/sshws.sh"
+wget -O status "https://raw.githubusercontent.com/scscp/Vergil/main2/status.sh"
+wget -O menu-backup "https://raw.githubusercontent.com/scscp/Vergil/main/backup/menu-backup.sh"
+wget -O backup "https://raw.githubusercontent.com/scscp/Vergil/main/backup/backup.sh"
+wget -O restore "https://raw.githubusercontent.com/scscp/Vergil/main/backup/restore.sh"
 
-wget -O jam "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/jam.sh"
-wget -O up "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/up.sh"
+wget -O jam "https://raw.githubusercontent.com/scscp/Vergil/main2/jam.sh"
+wget -O up "https://raw.githubusercontent.com/scscp/Vergil/main/up.sh"
 chmod +x menu
 chmod +x menu-trial
 chmod +x menu-vmess
